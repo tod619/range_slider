6f3d7f3078edcd147c6d97ee80482d0f70a72ed4
tod619 <trevorodwyer619@hotmail.com>
2021-04-24 22:05:31 +0100

    add eventlistner to range to change nums in label in script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+const range = document.getElementById('range')
+
+
+range.addEventListener('input', (e) => {
+    const value = +e.target.value
+    const label = e.target.nextElementSibling
+
+    label.innerText = value
+})
